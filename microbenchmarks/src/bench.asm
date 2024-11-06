0773adb6251ce4ef95bd80d8029f1aa12c5385b9
Pete Crowley <pc207@duke.edu>
2024-11-06 01:19:49 -0500

    finally good mbench

--- microbenchmarks/src/bench.asm
+++ microbenchmarks/src/bench.asm
@@ -1,12 +1,12 @@
 ; This is a simple assembly microbenchmark to show the effects of value prediction
-; It stores a value in memory, then flushes it out of the cache
-; Then it performs a bunch of no-ops to clear the pipeline
+; It stores a value in memory, then accesses it a few times to train the value predictor
+; Then it flushes the cache line and performs a bunch of no-ops to clear the pipeline
 ; Then it reloads the value from memory and performs some arithmetic operations with it
 ; Without value prediction, the processor has to stall to wait for these values before arithmetic operations can be performed
 ; With value prediction, the processor can predict the values and continue executing instructions without stalling
 
 section .data
-    my_int dd 42               ; Define a 32-bit integer with the initial value 42
+    my_int dd 100               ; Define a 32-bit integer with the initial value 42
 
 section .text
     global _start
@@ -14,17 +14,91 @@ section .text
 _start:
     ; Store a value in my_int
     mov eax, 100               ; Load the value to be stored in EAX
-    mov [my_int], eax          ; Store the value into memory (my_int)
+    mov [my_int], eax          ; Store the value into memory (my_int) so it goes to the l1 cache
+    mov ecx, 5
+
+    ;noops x50 to give the 
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+
+
+; train the value predictor
+_loop:
+    dec ecx
+    ; load value in
+    mov eax, [my_int]          ; Load the value from my_int into EAX
+    ; Do some addition with the value loaded from memory
+    add ebx, eax
+    imul ebx, eax
+
+    ; if ecx is greater than 1, jump back to _loop
+    cmp ecx, 1                  ; Compare ecx to 1
+    jg _loop                    ; If ecx > 1, jump back to _loop
+
+    cmp ecx, 0                  ; Compare ecx to 0
+    jz _end_loop                    ; If ecx == 0, jump back to _loop
+    
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
 
     ; Flush the cache line containing my_int
     clflush [my_int]              ; Flush the cache line containing my_int
-
-
-
-
-
-
-
 
     ; 185 no ops without a loop to make sure the pipeline is cleared
     nop
@@ -58,171 +132,10 @@ _start:
     nop
     nop
     nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
 
+    JMP _loop                       
 
-
-    ; Reload the value from memory into EBX
-    mov eax, [my_int]          ; Reload the value from my_int into EBX
-
-    ; Do some addition with the value loaded from memory
-    add ebx, eax
-    add ebx, eax
-    add ebx, eax
-    add ebx, eax                             
+_end_loop:
 
     ; Exit the program (for Linux)
     mov eax, 1                 ; sys_exit
