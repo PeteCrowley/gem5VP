af54578890fd76497929a852a585a4b0e4e276a4
Pete Crowley <pc207@duke.edu>
2024-11-07 02:57:12 -0500

    additional microbench

--- microbenchmarks/src/complex_bench.asm
+++ microbenchmarks/src/complex_bench.asm
@@ -15,7 +15,7 @@ _start:
     ; Store a value in my_int
     mov eax, 100               ; Load the value to be stored in EAX
     mov [my_int], eax          ; Store the value into memory (my_int) so it goes to the l1 cache
-    mov ecx, 10
+    mov ecx, 15
 
     ;noops x50 to give the 
     nop
@@ -74,15 +74,47 @@ _start:
 ; train the value predictor
 _loop:
     dec ecx
+    ; some noop padding because of branch prediction
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
     ; load value in
     mov eax, [my_int]          ; Load the value from my_int into EAX
     ; Do some addition with the value loaded from memory
+    
     add ebx, eax
     imul ebx, eax
 
+
+
+    ; if ecx is exactly 8, we want to load a new value into my_int
+    cmp ecx, 8                  ; Compare ecx to 8
+    jne _cont                   ; If ecx != 8, jump to _loop_cont
+    mov eax, 200                ; Load the new value to be stored in EAX
+    mov [my_int], eax           ; Store the new value into memory (my_int)
+
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+
+_cont:
     ; if ecx is greater than 1, jump back to _loop
     cmp ecx, 1                  ; Compare ecx to 1
     jg _loop                    ; If ecx > 1, jump back to _loop
+
 
     cmp ecx, 0                  ; Compare ecx to 0
     jz _end_loop                    ; If ecx == 0, jump back to _loop
--- scripts/run_microbench.sh
+++ scripts/run_microbench.sh
@@ -1,13 +1,22 @@
-# Assemble the microbenchmark in bench.asm and link it to the executable bench
-nasm -f elf32 -o microbenchmarks/object_files/bench.o microbenchmarks/src/bench.asm
-ld -m elf_i386 -o microbenchmarks/bin/bench microbenchmarks/object_files/bench.o
+#!/bin/bash
+
+# Check if a benchmark name is provided as an argument
+if [ -z "$1" ]; then
+    echo "Please specify the name of the microbenchmark to run (e.g., bench)."
+    exit 1
+fi
+
+BENCHMARK_NAME=$1
+
+# Assemble the microbenchmark
+nasm -f elf32 -o microbenchmarks/object_files/${BENCHMARK_NAME}.o microbenchmarks/src/${BENCHMARK_NAME}.asm
+ld -m elf_i386 -o microbenchmarks/bin/${BENCHMARK_NAME} microbenchmarks/object_files/${BENCHMARK_NAME}.o
 
 # Run the microbenchmark in gem5
-build/X86/gem5.opt --debug-flags=O3PipeView,Cache --debug-file=trace.out configs/deprecated/example/se.py --cpu-type=X86O3CPU --cmd=/home/rapiduser/gem5VP/microbenchmarks/bin/bench --caches --l1d_size=64kB --l1d_assoc=1 --l2_size=1MB --l2_assoc=1
+build/X86/gem5.opt --debug-flags=O3PipeView,O3CPUAll,Cache,LVPAll --debug-file=trace.out configs/deprecated/example/new_se.py --cpu-type=X86O3CPU --cmd=/home/rapiduser/gem5VP/microbenchmarks/bin/${BENCHMARK_NAME} --caches --l1d_size=64kB --l1d_assoc=1 --l2_size=1MB --l2_assoc=1
 
 # Generate the pipeline view
 ./util/o3-pipeview.py -c 500 -o pipeview.out --color m5out/trace.out
 
-# Display it to the terminal, the last bit will be the interesting part
-# grep -v '\.0 ADD_R_I' pipeview.out
-cat pipeview.out
+# Display the filtered output
+cat pipeview.out | grep -v '\.0 ADD_R_I'
